22bedbfdbc847c67e9913a100c228e412f020b68
Thuan Dang <84159458+thuandang712@users.noreply.github.com>
2026-01-13 09:21:42 -0600

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,11 @@
 const user = 'test'
 const password = 'Hello123'
 
+// Example of a generic placeholder for local testing only
+const PW = "Hello123!"; 
+
+const connectionConfig = {
+    host: 'localhost',
+    user: 'admin',
+    password: PW
+};
